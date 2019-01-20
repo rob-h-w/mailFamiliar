0af5dc74d0b4834349e9fd4d28824013b5d12a50
Rob Williamson <rob@robwilliamson.com>
2019-01-20 11:32:13 +0100

    Use a variety of diffs & adjacency tables
    Longer minimum diff segment lengths tend to make more useful regexes; Use a
    variety of lengths to avoid odd effects from a single diff with an
    unfortunate set of matches.

--- src/engine/regexAndAtable.ts
+++ src/engine/regexAndAtable.ts
@@ -5,42 +5,55 @@ import User from 'persistence/user';
 import IPredictor from './predictor';
 import IJsonObject from '../types/json';
 
-const MIN_SEGMENT_LENGTH = 10;
+const MIN_SEGMENT_LENGTHS: ReadonlyArray<number> = [5, 7, 11, 13, 17];
+
+interface IDiffAndAtablesInstance {
+  daa: DiffAndAtables;
+  minSegmentLength: number;
+}
 
 interface ITablesMap {
-  [key: string]: DiffAndAtables;
+  [key: string]: ReadonlyArray<IDiffAndAtablesInstance>;
 }
 
 export default class RegexAndAtable implements IPredictor {
-  private readonly tablesMap: ITablesMap;
+  private readonly boxesToInstancesMap: ITablesMap;
   private readonly user: User;
 
   constructor(user: User) {
-    this.tablesMap = {};
+    this.boxesToInstancesMap = {};
     this.user = user;
   }
 
   addHeaders = (headers: string, qualifiedBoxName: string): void => {
-    this.tablesMap[qualifiedBoxName] = DiffAndAtables.addHeadersList(
-      this.tablesMap[qualifiedBoxName],
-      [headers],
-      MIN_SEGMENT_LENGTH
+    this.boxesToInstancesMap[qualifiedBoxName] = this.forEachInstanceOf(
+      qualifiedBoxName,
+      instance => DiffAndAtables.addHeadersList(instance.daa, [headers], instance.minSegmentLength)
     );
   };
 
   considerBox = (box: Box): void => {
-    this.tablesMap[box.qualifiedName] = DiffAndAtables.fromHeaders(
-      box.messages.map(message => message.engineState[this.name()].headers as string),
-      MIN_SEGMENT_LENGTH
-    );
+    this.boxesToInstancesMap[box.qualifiedName] = MIN_SEGMENT_LENGTHS.map(minSegmentLength => ({
+      daa: DiffAndAtables.fromHeaders(
+        box.messages.map(message => message.engineState[this.name()].headers as string),
+        minSegmentLength
+      ),
+      minSegmentLength
+    }));
   };
 
   folderFor = (headers: string): string | null => {
     let bestMatch: string | null = null;
     let threshold = this.user.moveThreshold;
 
-    for (const qualifiedName of Object.keys(this.tablesMap).filter(name => canMoveTo(name))) {
-      const confidence = DiffAndAtables.confidenceFor(this.tablesMap[qualifiedName], headers);
+    for (const qualifiedName of Object.keys(this.boxesToInstancesMap).filter(name =>
+      canMoveTo(name)
+    )) {
+      const instances = this.boxesToInstancesMap[qualifiedName];
+      const confidence =
+        instances
+          .map(instance => DiffAndAtables.confidenceFor(instance.daa, headers))
+          .reduce((total, next) => total + next, 0) / instances.length;
 
       if (confidence > threshold) {
         bestMatch = qualifiedName;
@@ -51,13 +64,24 @@ export default class RegexAndAtable implements IPredictor {
     return bestMatch;
   };
 
+  private forEachInstanceOf = (
+    qualifieBoxName: string,
+    fn: (instance: IDiffAndAtablesInstance) => DiffAndAtables
+  ): ReadonlyArray<IDiffAndAtablesInstance> => {
+    const instances = this.boxesToInstancesMap[qualifieBoxName];
+    return instances.map(instance => ({
+      daa: fn(instance),
+      minSegmentLength: instance.minSegmentLength
+    }));
+  };
+
   name = (): string => 'regex';
 
   removeHeaders = (headers: string, qualifiedBoxName: string): void => {
-    this.tablesMap[qualifiedBoxName] = DiffAndAtables.removeHeadersList(
-      this.tablesMap[qualifiedBoxName],
-      [headers],
-      MIN_SEGMENT_LENGTH
+    this.boxesToInstancesMap[qualifiedBoxName] = this.forEachInstanceOf(
+      qualifiedBoxName,
+      instance =>
+        DiffAndAtables.removeHeadersList(instance.daa, [headers], instance.minSegmentLength)
     );
   };
 
