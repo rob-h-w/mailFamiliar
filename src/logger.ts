964b818a08ac9cfdf979b033949a7346840c66f3
Rob Williamson <rob@robwilliamson.com>
2019-04-07 16:29:36 +0200

    Remove linter warnings for logger.

--- src/logger.ts
+++ src/logger.ts
@@ -1,39 +1,40 @@
-const bunyan:any = require('bunyan');
-const file:any = require('fs');
-const path:any = require('path');
+import {createLogger} from 'bunyan';
+import {existsSync, mkdirSync} from 'fs';
+import {dirname, join} from 'path';
 
-declare const process:any;
-declare const require:Function;
+declare const process: any;
 
-const { name } = require('../package.json');
+import {name} from '../package.json';
 
 const LOG_DIR = 'logs';
 
 const LOG_STDOUT_LEVEL = process.env.LOG_STDOUT_LEVEL || 'debug';
 const LOG_FILE_LEVEL = process.env.LOG_FILE_LEVEL || 'warn';
-const LOG_FILE = process.env.LOG_FILE || path.join(process.cwd(), LOG_DIR, 'mailFamiliar.log');
-const LOG_FOLDER = path.dirname(LOG_FILE);
+const LOG_FILE = process.env.LOG_FILE || join(process.cwd(), LOG_DIR, 'mailFamiliar.log');
+const LOG_FOLDER = dirname(LOG_FILE);
 const LOG_ROTATE_PERIOD = process.env.LOG_ROTATE_PERIOD || '1d';
 const LOG_RETENTION_COUNT = process.env.LOG_RETENTION_COUNT || 3;
 
-if (!file.existsSync(LOG_FOLDER)) {
-  file.mkdirSync(LOG_DIR);
+if (!existsSync(LOG_FOLDER)) {
+  mkdirSync(LOG_DIR);
 }
 
-const logger = bunyan.createLogger({
+const logger = createLogger({
   name,
   src: true,
-  streams: [{
-    count: Number(LOG_RETENTION_COUNT),
-    level: LOG_FILE_LEVEL,
-    path: LOG_FILE,
-    period: LOG_ROTATE_PERIOD,
-    type: 'rotating-file'
-  },
-  {
-    level: LOG_STDOUT_LEVEL,
-    stream: process.stdout
-  }]
+  streams: [
+    {
+      count: Number(LOG_RETENTION_COUNT),
+      level: LOG_FILE_LEVEL,
+      path: LOG_FILE,
+      period: LOG_ROTATE_PERIOD,
+      type: 'rotating-file'
+    },
+    {
+      level: LOG_STDOUT_LEVEL,
+      stream: process.stdout
+    }
+  ]
 });
 
 export default logger;
