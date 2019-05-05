b45c7f56d55e4ded7e25c415f497b0670eeae55b
Rob Williamson <rob@robwilliamson.com>
2019-05-05 23:52:09 +0200

    Remove pre-build & delete steps.

--- test_and_report.sh
+++ test_and_report.sh
@@ -2,7 +2,6 @@
 
 yarn test:unit
 yarn test:api
-rm coverage/codeclimate*.json || true
 ./cc-test-reporter format-coverage -t lcov -o coverage/codeclimate.unit.json coverage/lcov.info
 ./cc-test-reporter format-coverage -t lcov -o coverage/codeclimate.api.json coverage/api.lcov.info
 ./cc-test-reporter sum-coverage coverage/codeclimate.api.json coverage/codeclimate.unit.json -p 2 -o coverage/codeclimate.json
