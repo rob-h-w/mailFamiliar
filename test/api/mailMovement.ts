52c8d7f17448ac2c73817f9462ed2ab03d166aed
Rob Williamson <rob@robwilliamson.com>
2019-12-07 22:14:51 +0100

    Update the mail movement test
    To cope with the time stamp of the fake message.

--- test/api/mailMovement.ts
+++ test/api/mailMovement.ts
@@ -2,14 +2,19 @@ import {expect} from 'code';
 const {afterEach, beforeEach, describe, it} = (exports.lab = require('lab').script());
 import * as _ from 'lodash';
 import * as mockery from 'mockery';
+import * as sinon from 'sinon';
 
 import mockImap, {MockResult as ImapMock} from './mocks/imap';
 import boxes from './tools/fixture/standard/boxes';
+import bunyan, {MockResult as BunyanMock} from './mocks/bunyan';
 import {useFixture} from './tools/fixture/standard/useFixture';
 import {EventHandlers, startServerInHealthyState} from './tools/server';
 import {fromBoxes} from './mocks/imap/serverState';
 import {mockStorageAndSetEnvironment} from './mocks/mailFamiliarStorage';
+import {until} from './tools/wait';
 
+let bunyanMock: BunyanMock;
+let clock: sinon.SinonFakeTimers;
 let imapMock: ImapMock;
 
 describe('mail movement', () => {
@@ -17,22 +22,50 @@ describe('mail movement', () => {
   let server: any;
 
   beforeEach(async () => {
-    await useFixture();
-
     mockery.enable({
       useCleanCache: true,
       warnOnReplace: false,
       warnOnUnregistered: false
     });
 
+    clock = sinon.useFakeTimers({
+      now: new Date('2019-01-01T00:00:00.000Z'),
+      shouldAdvanceTime: true
+    });
+
+    bunyanMock = bunyan();
+
+    mockery.registerMock('bunyan', bunyanMock.object);
+
+    await useFixture();
+
     mockStorageAndSetEnvironment();
 
     imapMock = mockImap();
-    imapMock.setServerState(fromBoxes(boxes));
+    const inbox = [
+      {
+        attributes: {
+          date: new Date('2018-12-25T12:21:37.000Z'),
+          flags: [],
+          size: 1234,
+          uid: 40465
+        },
+        body: Buffer.from("This goes in the buffer. It's buffer food. Nomnom."),
+        seqno: 40465,
+        synced: true
+      }
+    ];
+    const serverState = fromBoxes(boxes);
+    const inboxState = serverState.folders.INBOX;
+    inboxState.messages = inbox;
+    inboxState.messageState.total = inbox.length;
+
+    imapMock.setServerState(serverState);
 
     mockery.registerMock('imap', imapMock.class);
 
     ({eventHandlers, server} = await startServerInHealthyState(imapMock));
+    await until(() => bunyanMock.logger.info.calledWith(`shallow sync complete`));
   });
 
   afterEach(async () => {
@@ -41,6 +74,7 @@ describe('mail movement', () => {
       server = null;
     }
 
+    clock.restore();
     mockery.disable();
   });
 
@@ -54,7 +88,9 @@ describe('mail movement', () => {
   describe('when a mail from the opened mailbox is expunged', () => {
     beforeEach(async () => {
       imapMock.object.openBox.reset();
+      bunyanMock.logger.info.reset();
       await eventHandlers.on.expunge(40465);
+      await until(() => bunyanMock.logger.info.calledWith(`shallow sync complete`));
     });
 
     it('refreshes other mailboxes in case the message was moved', () => {
