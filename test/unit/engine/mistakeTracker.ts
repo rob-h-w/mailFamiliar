a11367dc09cd094bbc1782f9d5c8bd1ab8933b3c
Rob Williamson <rob@robwilliamson.com>
2020-01-26 22:17:32 +0100

    Error tracking implementation.

---
+++ test/unit/engine/mistakeTracker.ts
@@ -0,0 +1,94 @@
+import _ = require('lodash');
+import Sinon = require('sinon');
+
+import {expect} from '@hapi/code';
+const {beforeEach, describe, it} = (exports.lab = require('@hapi/lab').script());
+
+import Box from '../../../src/engine/box';
+import MistakeTracker from '../../../src/engine/mistakeTracker';
+import Move, {createMove} from '../../../src/types/move';
+import Mistake from 'types/mistake';
+
+describe('MistakeTracker', () => {
+  const headers: ReadonlyArray<string> = ['abc', 'def', 'ghi', 'jkl'];
+  const INBOX: string = 'INBOX';
+  const IMPORTANT: string = 'IMPORTANT';
+  const SPAM: string = 'SPAM';
+  const date: Date = new Date();
+
+  let boxen: Box[];
+  let moves: Move[];
+  let mistakeTracker: MistakeTracker;
+  let observer: Sinon.SinonStub;
+
+  beforeEach(() => {
+    boxen = [
+      new Box({
+        messages: [{date, headers: 'abc', seq: 1, uid: 2}],
+        name: INBOX,
+        qualifiedName: INBOX,
+        syncedTo: Date.now()
+      })
+    ];
+    moves = _.map(
+      _.zip([IMPORTANT, IMPORTANT, SPAM, SPAM], headers),
+      ([destination, headersString]) =>
+        createMove(
+          destination as string,
+          {date, headers: headersString as string, seq: 1, uid: 2},
+          date
+        )
+    );
+    observer = Sinon.stub();
+
+    mistakeTracker = new MistakeTracker(moves, boxen, (mistake: Mistake): void => {
+      observer(mistake);
+    });
+  });
+
+  it('provides an empty set of mistakes for unknown destinations', () => {
+    expect([mistakeTracker.mistakesFor('not there')]).to.equal([{}]);
+  });
+
+  it('lists mistakes for known destinations', () => {
+    expect([mistakeTracker.mistakesFor(IMPORTANT)]).to.equal([
+      {
+        abc: {
+          correctDestination: INBOX,
+          errantMove: createMove(IMPORTANT, {date, headers: 'abc', seq: 1, uid: 2}, date)
+        }
+      }
+    ]);
+    expect(observer.called).to.be.true();
+  });
+
+  it('discovers mistakes as new move results come in', () => {
+    expect([mistakeTracker.mistakesFor(SPAM)]).to.equal([{}]);
+    const message1 = {date, headers: 'ghi', seq: 1, uid: 2};
+    mistakeTracker.inspectMessage(INBOX, message1);
+    expect([mistakeTracker.mistakesFor(SPAM)]).to.equal([
+      {
+        ghi: {
+          correctDestination: INBOX,
+          errantMove: createMove(SPAM, message1, date)
+        }
+      }
+    ]);
+    expect(observer.called).to.be.true();
+    const message2 = {date, headers: 'mno', seq: 1, uid: 2};
+    mistakeTracker.addMove(createMove(SPAM, message2, date));
+    mistakeTracker.inspectMessage(IMPORTANT, message2);
+    expect([mistakeTracker.mistakesFor(SPAM)]).to.equal([
+      {
+        ghi: {
+          correctDestination: INBOX,
+          errantMove: createMove(SPAM, message1, date)
+        },
+        mno: {
+          correctDestination: IMPORTANT,
+          errantMove: createMove(SPAM, message2, date)
+        }
+      }
+    ]);
+  });
+});
