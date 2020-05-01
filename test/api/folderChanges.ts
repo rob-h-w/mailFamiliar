c2646c1175c80364205e8a071794824939ef098d
Rob Williamson <rob@robwilliamson.com>
2020-05-01 14:20:36 +0200

    Stub out process.exit to ensure there is always test output
    Even if we exit the process erroneously. This only prevents us from
    terminating the process after test initialization has happened.

--- test/api/folderChanges.ts
+++ test/api/folderChanges.ts
@@ -1,18 +1,26 @@
 import {expect} from '@hapi/code';
-const {afterEach, beforeEach, describe, it} = (exports.lab = require('@hapi/lab').script());
+const {
+  after,
+  afterEach,
+  before,
+  beforeEach,
+  describe,
+  it,
+} = (exports.lab = require('@hapi/lab').script());
 import * as mockery from 'mockery';
 import * as sinon from 'sinon';
 
 import {Box} from 'imap';
 
+import bunyan, {MockResult as BunyanMock} from './mocks/bunyan';
 import mockImap from './mocks/imap';
 import ImapMock from './mocks/imap/mockResult';
 import ServerState, {fromBoxes} from './mocks/imap/serverState';
+import {mockStorageAndSetEnvironment} from './mocks/mailFamiliarStorage';
 import boxes from './tools/fixture/standard/boxes';
-import bunyan, {MockResult as BunyanMock} from './mocks/bunyan';
 import {useFixture} from './tools/fixture/standard/useFixture';
 import {startServerInHealthyState} from './tools/server';
-import {mockStorageAndSetEnvironment} from './mocks/mailFamiliarStorage';
+import stubExit from './tools/stubExit';
 import {until} from './tools/wait';
 
 let bunyanMock: BunyanMock;
@@ -21,6 +29,8 @@ let imapMock: ImapMock;
 
 const INBOX = 'INBOX';
 const INTERESTING_SPAM = 'Interesting spam';
+
+stubExit(before, after);
 
 describe('folder', () => {
   const inbox = [
--- test/api/folderSelection.ts
+++ test/api/folderSelection.ts
@@ -1,5 +1,12 @@
 import {expect} from '@hapi/code';
-const {afterEach, beforeEach, describe, it} = (exports.lab = require('@hapi/lab').script());
+const {
+  after,
+  afterEach,
+  before,
+  beforeEach,
+  describe,
+  it,
+} = (exports.lab = require('@hapi/lab').script());
 import * as _ from 'lodash';
 import * as mockery from 'mockery';
 import * as sinon from 'sinon';
@@ -10,15 +17,18 @@ import fakeBox from './mocks/imap/fakeBox';
 import {default as ImapMock} from './mocks/imap/mockResult';
 import ServerState, {fromBoxes} from './mocks/imap/serverState';
 import {mockStorageAndSetEnvironment} from './mocks/mailFamiliarStorage';
+import {PredictorTypeValues} from '../../src/engine/predictors';
 import boxes from './tools/fixture/standard/boxes';
 import {useFixture} from './tools/fixture/standard/useFixture';
 import {startServerInHealthyState} from './tools/server';
+import stubExit from './tools/stubExit';
 import {waitATick, until} from './tools/wait';
-import {PredictorTypeValues} from '../../src/engine/predictors';
 
 let bunyanMock: BunyanMock;
 let clock: sinon.SinonFakeTimers;
 let imapMock: ImapMock;
+
+stubExit(before, after);
 
 describe('folder selection', () => {
   const inbox = [
@@ -27,12 +37,12 @@ describe('folder selection', () => {
         date: new Date('2018-12-25T12:21:37.000Z'),
         flags: [],
         size: 1234,
-        uid: 41
+        uid: 41,
       },
       body: Buffer.from('whut up?'),
       seqno: 41,
-      synced: true
-    }
+      synced: true,
+    },
   ];
   const interestingSpam = [
     {
@@ -40,34 +50,34 @@ describe('folder selection', () => {
         date: new Date('2018-12-26T01:09:55.000Z'),
         flags: [],
         size: 15711,
-        uid: 68
+        uid: 68,
       },
       body: Buffer.from('interesting spam like this'),
       seqno: 68,
-      synced: true
+      synced: true,
     },
     {
       attributes: {
         date: new Date('2018-12-27T00:47:48.000Z'),
         flags: [],
         size: 3688,
-        uid: 69
+        uid: 69,
       },
       body: Buffer.from('interesting spam like that'),
       seqno: 69,
-      synced: true
+      synced: true,
     },
     {
       attributes: {
         date: new Date('2018-12-27T12:48:50.000Z'),
         flags: [],
         size: 3655,
-        uid: 70
+        uid: 70,
       },
       body: Buffer.from('interesting spam like this'),
       seqno: 70,
-      synced: true
-    }
+      synced: true,
+    },
   ];
 
   let server: any;
@@ -77,13 +87,13 @@ describe('folder selection', () => {
     mockery.enable({
       useCleanCache: true,
       warnOnReplace: false,
-      warnOnUnregistered: false
+      warnOnUnregistered: false,
     });
 
     bunyanMock = bunyan();
     clock = sinon.useFakeTimers({
       now: new Date('2019-01-01T00:00:00.000Z'),
-      shouldAdvanceTime: true
+      shouldAdvanceTime: true,
     });
     mockery.registerMock('bunyan', bunyanMock.object);
   });
@@ -93,7 +103,7 @@ describe('folder selection', () => {
     mockery.disable();
   });
 
-  PredictorTypeValues.alternatives.forEach(predictorType =>
+  PredictorTypeValues.alternatives.forEach((predictorType) =>
     describe(`with predictor ${predictorType.value}`, () => {
       beforeEach(async () => {
         await useFixture();
@@ -143,12 +153,12 @@ describe('folder selection', () => {
                   attributes: {
                     date: new Date(),
                     flags: [],
-                    uid: 32
+                    uid: 32,
                   },
                   body: Buffer.from('interesting spam like the others'),
                   seqno: 1,
-                  synced: false
-                }
+                  synced: false,
+                },
               ]);
               await until(() => bunyanMock.logger.debug.calledWith('New mails handled'));
             });
@@ -169,12 +179,12 @@ describe('folder selection', () => {
                     attributes: {
                       date: new Date(),
                       flags: ['\\Seen'],
-                      uid: 33
+                      uid: 33,
                     },
                     body: Buffer.from('interesting spam like the others'),
                     seqno: 2,
-                    synced: false
-                  }
+                    synced: false,
+                  },
                 ]);
                 await until(() => bunyanMock.logger.debug.calledWith('New mails handled'));
               });
@@ -193,12 +203,12 @@ describe('folder selection', () => {
                 attributes: {
                   date: new Date(),
                   flags: [],
-                  uid: 32
+                  uid: 32,
                 },
                 body: Buffer.from("what's this now?"),
                 seqno: 2,
-                synced: false
-              }
+                synced: false,
+              },
             ]);
             await imapMock.eventHandlers.on.mail(1);
             await waitATick();
@@ -237,10 +247,10 @@ describe('folder selection', () => {
             'this little piggy stayed home',
             'this little piggy had roast beef',
             'and this little piggy had none',
-            'aaand this little piggy went wee wee wee all the way home'
+            'aaand this little piggy went wee wee wee all the way home',
           ]),
-          SPAM: fakeBox(['shouty spamulation', 'more spam'])
-        }
+          SPAM: fakeBox(['shouty spamulation', 'more spam']),
+        },
       };
       UNSORTED = {
         currentlyOpenBox: null,
@@ -252,11 +262,11 @@ describe('folder selection', () => {
             'this little piggy stayed home',
             'this little piggy had roast beef',
             'and this little piggy had none',
-            'aaand this little piggy went wee wee wee all the way home'
+            'aaand this little piggy went wee wee wee all the way home',
           ]),
           PIGGIES: fakeBox([]),
-          SPAM: fakeBox([])
-        }
+          SPAM: fakeBox([]),
+        },
       };
 
       mockStorageAndSetEnvironment({predictorType: 'Traat'}, 'rob');
@@ -271,7 +281,7 @@ describe('folder selection', () => {
       bunyanMock.logger.info.reset();
       bunyanMock.logger.debug.reset();
 
-      UNSORTED.folders.INBOX.messages.forEach(msg => imapMock.simulate.event.expunge(msg.seqno));
+      UNSORTED.folders.INBOX.messages.forEach((msg) => imapMock.simulate.event.expunge(msg.seqno));
       imapMock.setServerState({...SORTED, currentlyOpenBox: 'INBOX'});
       await until(() => bunyanMock.logger.debug.calledWith('Opened INBOX'));
       bunyanMock.logger.info.reset();
@@ -290,12 +300,12 @@ describe('folder selection', () => {
             attributes: {
               date: new Date(),
               flags: [],
-              uid: 32
+              uid: 32,
             },
             body: Buffer.from('this little piggy had an existential crisis'),
             seqno: 1,
-            synced: false
-          }
+            synced: false,
+          },
         ]);
 
         await until(() => bunyanMock.logger.debug.calledWith('New mails handled'));
