bcc2d7346899f296165f3ba035f895c81ee0fa7a
Rob Williamson <rob@robwilliamson.com>
2020-12-06 10:11:12 +0100

    Publish a folder synchronized event.

--- java/src/test/java/com/robwilliamson/mailfamiliar/service/imap/SynchronizerTest.java
+++ java/src/test/java/com/robwilliamson/mailfamiliar/service/imap/SynchronizerTest.java
@@ -2,22 +2,24 @@ package com.robwilliamson.mailfamiliar.service.imap;
 
 import com.robwilliamson.mailfamiliar.config.ImapSync;
 import com.robwilliamson.mailfamiliar.entity.User;
+import com.robwilliamson.mailfamiliar.events.FolderSynchronized;
 import com.robwilliamson.mailfamiliar.exceptions.*;
 import com.robwilliamson.mailfamiliar.repository.*;
 import com.robwilliamson.mailfamiliar.service.CryptoService;
 import com.robwilliamson.mailfamiliar.service.imap.events.*;
-import org.flywaydb.test.FlywayTestExecutionListener;
-import org.flywaydb.test.annotation.FlywayTest;
+import com.robwilliamson.test.EventReceiver;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.*;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.mail.*;
 import javax.mail.event.FolderEvent;
@@ -30,16 +32,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@FlywayTest
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-    MockitoTestExecutionListener.class,
-    FlywayTestExecutionListener.class})
 class SynchronizerTest {
   Synchronizer subject;
   User user;
   @Autowired
   CryptoService cryptoService;
+  @Autowired
+  Flyway flyway;
   @Autowired
   ImapSync imapSync;
   @Autowired
@@ -52,6 +53,8 @@ class SynchronizerTest {
   MessageChannel imapEventChannel;
   @MockBean
   StoreFactory storeFactory;
+  @MockBean
+  EventReceiver synchronizedEventReceiver;
   @Mock
   Folder defaultFolder;
   @Mock
@@ -63,23 +66,23 @@ class SynchronizerTest {
   @Autowired
   TaskExecutor taskExecutor;
   FolderRemoved folderRemoved;
-  FolderSynchronized folderSynchronized;
   @Autowired
   private EncryptedRepository encryptedRepository;
   @Autowired
   private UserRepository userRepository;
 
   private void mockFolder(Folder folder, String name) throws MessagingException {
-    when(folder.getType()).thenReturn(HOLDS_FOLDERS | HOLDS_MESSAGES);
-    when(folder.list()).thenReturn(new Folder[0]);
-    when(folder.getMessages()).thenReturn(new Message[0]);
-    when(folder.getName()).thenReturn(name);
-    when(folder.getParent()).thenReturn(defaultFolder);
+    lenient().when(folder.getType()).thenReturn(HOLDS_FOLDERS | HOLDS_MESSAGES);
+    lenient().when(folder.list()).thenReturn(new Folder[0]);
+    lenient().when(folder.getMessages()).thenReturn(new Message[0]);
+    lenient().when(folder.getName()).thenReturn(name);
+    lenient().when(folder.getParent()).thenReturn(defaultFolder);
   }
 
   @BeforeEach
-  @FlywayTest
   void setUp() throws MessagingException, MissingUserException, MissingSecretException {
+    flyway.clean();
+    flyway.migrate();
     user = fakeUser(cryptoService, encryptedRepository, userRepository);
     final var secret = fakeUserSecret(user, cryptoService, encryptedRepository);
     final var imap = imapAccountEntity("imap.account");
@@ -88,19 +91,11 @@ class SynchronizerTest {
     imap.setPassword(secret.getId());
     imap.setSyncPeriodDays(30);
     mockFolder(inbox, "INBOX");
-    when(defaultFolder.getType()).thenReturn(HOLDS_FOLDERS);
-    when(defaultFolder.list()).thenReturn(new Folder[]{inbox});
-    when(defaultFolder.getName()).thenReturn("");
+    lenient().when(defaultFolder.getType()).thenReturn(HOLDS_FOLDERS);
+    lenient().when(defaultFolder.list()).thenReturn(new Folder[]{inbox});
+    lenient().when(defaultFolder.getName()).thenReturn("");
     when(store.getDefaultFolder()).thenReturn(defaultFolder);
     doReturn(store).when(storeFactory).getInstance(any(), any());
-    when(imapEventChannel.send(any(FolderSynchronized.class)))
-        .thenAnswer((Answer<Void>) invocationOnMock -> {
-          final var event = invocationOnMock.getArguments()[0];
-          if (event instanceof FolderSynchronized) {
-            folderSynchronized = (FolderSynchronized) event;
-          }
-          return null;
-        });
     when(imapEventChannel.send(any(FolderRemoved.class)))
         .thenAnswer((Answer<Void>) invocationOnMock -> {
           final var event = invocationOnMock.getArguments()[0];
@@ -109,13 +104,17 @@ class SynchronizerTest {
           }
           return null;
         });
-    subject = imapSync.createSynchronizer(imap);
+    subject = imapSync.createSynchronizer(imap, taskExecutor);
   }
 
   @AfterEach
-  void tearDown() {
+  void tearDown() throws InterruptedException {
+    subject.close();
+    until(() -> ((ThreadPoolTaskExecutor) taskExecutor).getActiveCount() == 2);
+    reset(synchronizedEventReceiver);
     reset(imapEventChannel);
     reset(storeFactory);
+    flyway.clean();
   }
 
   @Nested
@@ -145,7 +144,7 @@ class SynchronizerTest {
   }
 
   @Nested
-  class FolderCreated {
+  class ImapEvent {
     @Nested
     class WithIgnoredFolder {
       @Mock
@@ -170,13 +169,25 @@ class SynchronizerTest {
 
       @BeforeEach
       void setUp() throws MessagingException, InterruptedException {
-        folderSynchronized = null;
         mockFolder(storable, "storable");
+        ArgumentCaptor<Object> synchronizedArgumentCaptor =
+            ArgumentCaptor.forClass(Object.class);
         message1 = mockMessage("from@mail.com", "to@mail.com");
         when(storable.getMessageCount()).thenReturn(1);
         when(storable.getMessage(1)).thenReturn(message1);
         subject.folderCreated(new FolderEvent(new Object(), storable, CREATED));
-        until(() -> folderSynchronized != null);
+        until(() -> {
+          verify(synchronizedEventReceiver, atLeast(0))
+              .onEvent(synchronizedArgumentCaptor.capture());
+          System.out.println(synchronizedArgumentCaptor.getAllValues()
+              .stream()
+              .filter(event -> event instanceof FolderSynchronized)
+              .count());
+          return synchronizedArgumentCaptor.getAllValues()
+              .stream()
+              .filter(event -> event instanceof FolderSynchronized)
+              .count() > 0;
+        });
       }
 
       @Test
