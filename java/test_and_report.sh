f87aa32815259c809b18204a25463f9077f78ff3
Rob Williamson <rob@robwilliamson.com>
2020-12-28 10:46:20 +0100

    Tweak test report paths.

--- java/test_and_report.sh
+++ java/test_and_report.sh
@@ -4,11 +4,12 @@ set -e
 root=$(dirname $(readlink -f "$0"))
 pushd $root
 reporter=$root/../cc-test-reporter
-export JACOCO_SOURCE_PATH=$root/src/main/java/
+export JACOCO_SOURCE_PATH=src/main/java
 
 $reporter before-build
 
 ./mvnw test jacoco:report -B
 
-$reporter format-coverage $root/target/site/jacoco/jacoco.xml -t jacoco --add-prefix "${root}/src/main/java/"
+$reporter format-coverage $root/target/site/jacoco/jacoco.xml -t jacoco --add-prefix \
+  "src/main/java"
 $reporter upload-coverage -i coverage/codeclimate.json
