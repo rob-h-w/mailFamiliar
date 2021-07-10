b5031282dd2b29a5440d09a4f781663ec5108b50
Rob Williamson <rob@robwilliamson.com>
2021-07-10 12:11:00 +0200

    The builder package name changed

--- java/src/main/java/com/robwilliamson/mailfamiliar/Equals.java
+++ java/src/main/java/com/robwilliamson/mailfamiliar/Equals.java
@@ -1,6 +1,6 @@
 package com.robwilliamson.mailfamiliar;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 
 import java.util.function.BiFunction;
 
